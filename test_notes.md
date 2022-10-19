5e429a07796f5e969538268fd4c444d03a631e76
Alistair Walsh <alistair.walsh@me.com>
2022-10-19 14:16:02 +1100

    with download

--- test_notes.md
+++ test_notes.md
@@ -1,2 +1,4 @@
 dates in excel format
 some Na as unknown
+
+modied_by is not always there
--- utils/data_handler.py
+++ utils/data_handler.py
@@ -1,16 +1,21 @@
 
-def slicer(day):
+def slicer(df):
     '''
     finds the column 'created' and uses that to split forms
     '''
-
-    created_list = day.filter(regex='created').columns.tolist()
-
-    return [day.loc[:,i:ii].columns.tolist()[:-1] for i,ii in zip(created_list, created_list[1:])]
+    created_list = df.filter(regex='created').columns.tolist()
+    return [df.loc[:,i:ii].columns.tolist()[1:-1] for i,ii in zip(created_list, created_list[1:])]
 
 def dropper(df):
-    c_drop = df.filter(regex = 'modified').columns
+    c_drop = df.filter(regex = 'modified').columns #selects modified_by as well
     return df.drop(columns = c_drop)
+
+df = pd.read_csv("0.88/browser_data.csv")
+
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+
+
 
 
 #not finished
