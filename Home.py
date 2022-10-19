1dc13230a091242715d5b00c278eddf64c1c91a6
Alistair Walsh <alistair.walsh@me.com>
2022-10-19 12:22:55 +1100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import streamlit as st
+from atom import ATOMClassifier
+# Expand the web app across the whole screen
+st.set_page_config(layout="wide")
 
+st.sidebar.title("Pipeline")
+
+# Data cleaning options
+st.sidebar.subheader("Data cleaning")
+scale = st.sidebar.checkbox("Scale", False, "scale")
+encode = st.sidebar.checkbox("Encode", False, "encode")
+impute = st.sidebar.checkbox("Impute", False, "impute")
