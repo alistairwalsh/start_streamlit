e81422d565701b5e88ab7e2f9b717457ade3e319
Alistair Walsh <alistair.walsh@me.com>
2022-10-19 13:24:46 +1100

    got it I think

--- Home.py
+++ Home.py
@@ -30,10 +30,12 @@ uploadedfiles = st.file_uploader("Upload data:", type=['xlsx'],accept_multiple_f
 st.write(uploadedfiles)
 
 [st.write(file.name) for file in uploadedfiles]
+
+
 #If a dataset is uploaded, show a preview
 if uploadedfiles is not None:
-    for n,f in enumerate(uploadedfiles):
+    for f in uploadedfiles:
         data = pd.read_excel(f)
-        st.write(n)
+        st.write(f.name)
         st.text("Data preview:")
         st.dataframe(data.head())
