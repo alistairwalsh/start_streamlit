875982053b395e412a4feec05b0c98eb896fd8ef
Alistair Walsh <alistair.walsh@me.com>
2022-10-25 08:56:39 +1100

    multiselect

--- pages/START_clinical_data_summary.py
+++ pages/START_clinical_data_summary.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-option = st.selectbox(
+option = st.multiselect(
     'Which timepoints?',
-    ('Baseline', 'T=0', '12-24 hours'))
+    ('Baseline', 'T=0', '12-24 hours','Day 3-7','90 days','12 months'))
 
 st.write('You selected:', option)
 
