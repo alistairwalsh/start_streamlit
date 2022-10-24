1920fd8e774d070692f4b1d36423e1ddb4cde001
Alistair Walsh <alistair.walsh@me.com>
2022-10-25 07:17:59 +1100

    formatting

--- pages/START_clinical_data_summary.py
+++ pages/START_clinical_data_summary.py
@@ -3,16 +3,16 @@ import streamlit as st
 st.write('''
 START-PrePARE
 (sub-study and stand alone)
-Baseline
-(recruitment up to 3 days post-stroke)
-T=0
-(2-3 hours after baseline if recruited <24 hrs)
-12-24 hours
-(12-24 hrs post T=0 or 12-24 hrs for those recruited > 24 hrs post stroke and < 48 hrs)
-Day 3-7
-(3-7 days post stroke onset, can be the same time point at recruitment)
-90 days
-(+/- 7 days)
-12 months
+st.write('''Baseline
+(recruitment up to 3 days post-stroke)''')
+st.write('''T=0
+(2-3 hours after baseline if recruited <24 hrs)''')
+st.write('''12-24 hours
+(12-24 hrs post T=0 or 12-24 hrs for those recruited > 24 hrs post stroke and < 48 hrs)''')
+st.write('''Day 3-7
+(3-7 days post stroke onset, can be the same time point at recruitment)''')
+st.write('''90 days
+(+/- 7 days)''')
+st.write('''12 months
 (+/- 7 days)''')
 
