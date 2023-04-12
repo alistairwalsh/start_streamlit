b77b3ed384a7dbac1aa235f9f6b61db913a50f07
Alistair Walsh <alistair.walsh@me.com>
2023-04-12 12:56:18 +1000

    add to google sheets

--- pages/from_google_sheets.py
+++ pages/from_google_sheets.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 from google.oauth2 import service_account
 from gsheetsdb import connect
+import pandas as pd
+import pandas_profiling
+import streamlit as st
+
+from streamlit_pandas_profiling import st_profile_report
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
@@ -26,9 +31,12 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 #st.write(type(rows))
 
 #st.write(rows)
-
-st.dataframe(pd.DataFrame(rows))
+df = pd.DataFrame(rows)
+st.dataframe(df)
 
 # # Print results.
 # for row in rows:
 #     st.write(f"{row.name} has a :{row.pet}:")
+pr = df.profile_report()
+
+st_profile_report(pr)
