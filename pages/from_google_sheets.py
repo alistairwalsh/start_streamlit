99522cd6f50d989841d4d3bf7ee06e44556c3aab
Alistair Walsh <alistair.walsh@me.com>
2023-04-12 13:01:34 +1000

    tidy google sheets

--- pages/from_google_sheets.py
+++ pages/from_google_sheets.py
@@ -2,10 +2,7 @@ import streamlit as st
 import pandas as pd
 from google.oauth2 import service_account
 from gsheetsdb import connect
-import pandas as pd
 import pandas_profiling
-import streamlit as st
-
 from streamlit_pandas_profiling import st_profile_report
 
 # Create a connection object.
@@ -28,15 +25,6 @@ def run_query(query):
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-#st.write(type(rows))
-
-#st.write(rows)
 df = pd.DataFrame(rows)
-st.dataframe(df)
-
-# # Print results.
-# for row in rows:
-#     st.write(f"{row.name} has a :{row.pet}:")
 pr = df.profile_report()
-
 st_profile_report(pr)
