e9937aeee3152abde020ee90c553c2efb9a2703b
Alistair Walsh <alistair.walsh@me.com>
2023-04-27 11:02:22 +1000

    stop profiling for now

--- pages/pandas_profiling.py
+++ pages/pandas_profiling.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import pandas_profiling
+#import pandas_profiling
 import streamlit as st
 
-from streamlit_pandas_profiling import st_profile_report
+#from streamlit_pandas_profiling import st_profile_report
 
-df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
-pr = df.profile_report()
+#df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
+#pr = df.profile_report()
 
-st_profile_report(pr)
+#st_profile_report(pr)
